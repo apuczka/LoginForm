082df8b734843f58bbad159fdad883be7feb927d
apuczka <a.puczka@icloud.com>
2023-02-09 10:49:00 +0100

    useContext

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import AuthContext from '../../contex/auth-contex';
 
 import classes from './Navigation.module.css';
 
 const Navigation = (props) => {
+const ctx = useContext(AuthContext)
+
   return (
-    <AuthContext.Consumer>
-    {(ctx) => {
-      return (
+   
         <nav className={classes.nav}>
       <ul>
         {ctx.isLoggedIn && (
@@ -27,10 +27,9 @@ const Navigation = (props) => {
         )}
       </ul>
     </nav>
-      )
-    }}
+      
+  
     
-    </AuthContext.Consumer>
   );
 };
 
