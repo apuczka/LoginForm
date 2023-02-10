e0a5524ee7d27a3297258554c3b5487b1b29d904
apuczka <a.puczka@icloud.com>
2023-02-10 12:33:57 +0100

    Context Provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 import App from './App';
+import { AuthContexProvider } from './contex/auth-contex';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <AuthContexProvider>
+  <App />
+  </AuthContexProvider>);
